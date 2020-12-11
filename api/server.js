1f95641674d642c1b89cb285db875b7fb06a2e3e
Gerardo Rivera <gerardo.rivera3901@gmail.com>
2020-12-11 08:39:55 -0800

    Initial Commit

--- api/server.js
+++ api/server.js
@@ -1,1 +1,16 @@
-// build your server here
+const express = require('express');
+const ProjectRouter = require('./project/router');
+const ResourceRouter = require('./resource/router');
+const TaskRouter = require('./task/router');
+const server = express();
+
+server.use(express.json());
+server.use('/api/projects', ProjectRouter);
+server.use('/api/resources', ResourceRouter);
+server.use('/api/tasks', TaskRouter);
+
+server.get('/', (req, res) => {
+  res.status(200).json({ api: "UP!" });
+});
+
+module.exports = server;
